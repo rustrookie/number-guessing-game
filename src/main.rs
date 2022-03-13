45787b3f505b4e7444f6fd3eccf81336160266c5
James Sinkala <xinnks@gmail.com>
2022-03-13 03:04:11 +0300

    feat: Display game title and instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    println!("'Guess The Number' Game!");
+    println!("In this game you'll be guessing the secret number between a range of numbers, the lower being inclusive while the higher being exclusive. To start provide the lower and higher limits of the range of numbers.");
 }
