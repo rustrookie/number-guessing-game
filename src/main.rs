cedde7733a33192d260f7a9ce0e10e956db747cd
James Sinkala <xinnks@gmail.com>
2022-03-13 03:50:53 +0300

    feat: Loop through the player's guesses printing out a message that reflects the possible different states.

--- src/main.rs
+++ src/main.rs
@@ -32,4 +32,38 @@ fn main(){
     };
 
     let secret_number = rand::thread_rng().gen_range(_lower_limit.._higher_limit);
+
+    let mut _moves_made: u32 = 0;
+
+    loop {
+        println!("Input guess: ");
+
+        let mut _guess = String::new();
+
+        io::stdin()
+            .read_line(&mut _guess)
+            .expect("Failed to read input!!!");
+
+        let _guess: u32 = match _guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed the number: {}", _guess);
+
+        _moves_made += 1;
+
+        match _guess.cmp(&secret_number) {
+            Ordering::Less  => println!("Too small!!"),
+            Ordering::Greater  => println!("Too big!!"),
+            Ordering::Equal  => {
+                let total_moves = _higher_limit - _lower_limit;
+                let total_moves_float = total_moves as f64;
+                let moves_made_float = _moves_made as f64;
+                let efficiency: f64 = ((total_moves_float - moves_made_float) / total_moves_float) * 100.00;
+                println!("YOU HAVE WON!! \n Guesses Made: {}\n Possible Guesses: {}\n Efficiency: {}%" , _moves_made, total_moves, efficiency);
+                break;
+            }
+        }
+    }
 }
