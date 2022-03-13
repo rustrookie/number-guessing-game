0b7a6d777db2f5f0702674dd6965e11e7ec38d86
James Sinkala <xinnks@gmail.com>
2022-03-13 03:37:43 +0300

    feat: Get first user input

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,16 @@ use std::io;
 fn main(){
     println!("'Guess The Number' Game!");
     println!("In this game you'll be guessing the secret number between a range of numbers, the lower being inclusive while the higher being exclusive. To start provide the lower and higher limits of the range of numbers.");
+
+    let mut _lower_limit = String::new();
+    
+    println!("Choose the lower limit (number > 0): ");
+
+    io::stdin().read_line(&mut _lower_limit)
+    .expect("Failed to read value");
+
+    let _lower_limit: u32 = match _lower_limit.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 1,
+    };
 }
