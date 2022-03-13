6545a503f15a40860d6af86ba80d704d383bd472
James Sinkala <xinnks@gmail.com>
2022-03-13 03:04:58 +0300

    feat: Use the io prelude

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main(){
     println!("'Guess The Number' Game!");
     println!("In this game you'll be guessing the secret number between a range of numbers, the lower being inclusive while the higher being exclusive. To start provide the lower and higher limits of the range of numbers.");
